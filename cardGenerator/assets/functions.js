48850d37c37c57982919d19094a49df62ce3a102
galarist <galamboscristovao@gmail.com>
2021-08-05 01:48:52 +1000

    Simplified JS

--- cardGenerator/assets/functions.js
+++ cardGenerator/assets/functions.js
@@ -3,10 +3,9 @@ function generate(params) {
     let myText = document.getElementById('profileName');
     myText.innerHTML = myOwnText.value // Update the text with the new value
 
-    var theInput = document.getElementById("color");
-    var theColor = theInput.value;
+    var theInput = document.getElementById("color").value;
     var cardBG = document.getElementById("card");
-    cardBG.style.background = theColor; // Update background color for the card
+    cardBG.style.background = theInput; // Update background color for the card
 
     var img = document.getElementById("output");
     if (document.getElementById("profilePic").files.length !== 0) { 
